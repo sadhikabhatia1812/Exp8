ee7cac162697a00c37986e1d919e389b329536fc
sadhikabhatia1812 <artermis1806@gmail.com>
2025-08-21 14:35:54 +0530

    Create addition of two numbers.cpp

---
+++ addition of two numbers.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int r, c;
+    cout << "Enter number of rows: ";
+    cin >> r;
+    cout << "Enter number of columns: ";
+    cin >> c;
+
+    int a[10][10], b[10][10], sum[10][10];
+
+    cout << "Enter elements of first matrix (put space between numbers):\n";
+    for (int i = 0; i < r; i++) {
+        for (int j = 0; j < c; j++) {
+            cin >> a[i][j];
+        }
+    }
+
+    cout << "Enter elements of second matrix (put space between numbers):\n";
+    for (int i = 0; i < r; i++) {
+        for (int j = 0; j < c; j++) {
+            cin >> b[i][j];
+        }
+    }
+
+    for (int i = 0; i < r; i++) {
+        for (int j = 0; j < c; j++) {
+            sum[i][j] = a[i][j] + b[i][j];
+        }
+    }
+
+    cout << "Sum of matrices:\n";
+    for (int i = 0; i < r; i++) {
+        for (int j = 0; j < c; j++) {
+            cout << sum[i][j] << " ";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
