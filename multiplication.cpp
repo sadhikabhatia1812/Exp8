83a5977de727ff4db7b352dd21de61407720fd58
sadhikabhatia1812 <artermis1806@gmail.com>
2025-08-21 14:38:30 +0530

    Create multiplication.cpp

---
+++ multiplication.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int r1, c1, r2, c2;
+    cout << "Enter rows and columns of first matrix: (put space between numbers)\n ";
+    cin >> r1 >> c1;
+    cout << "Enter rows and columns of second matrix: (put space between numbers)\n";
+    cin >> r2 >> c2;
+
+    if (c1 != r2) {
+        cout << "Matrix multiplication not possible.";
+        return 0;
+    }
+
+    int a[10][10], b[10][10], mul[10][10] = {0};
+
+    cout << "Enter elements of first matrix: (put space between numbers)\n";
+    for (int i = 0; i < r1; i++) {
+        for (int j = 0; j < c1; j++) {
+            cin >> a[i][j];
+        }
+    }
+
+    cout << "Enter elements of second matrix: (put space between numbers)\n";
+    for (int i = 0; i < r2; i++) {
+        for (int j = 0; j < c2; j++) {
+            cin >> b[i][j];
+        }
+    }
+
+    for (int i = 0; i < r1; i++) {
+        for (int j = 0; j < c2; j++) {
+            for (int k = 0; k < c1; k++) {
+                mul[i][j] += a[i][k] * b[k][j];
+            }
+        }
+    }
+
+    cout << "Result of multiplication:\n";
+    for (int i = 0; i < r1; i++) {
+        for (int j = 0; j < c2; j++) {
+            cout << mul[i][j] << " ";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
